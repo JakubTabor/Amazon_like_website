51dfba719c1c0cf75ead33d0ed623042fc9f4a5d
Jakub <124591894+JakubTabor@users.noreply.github.com>
2024-04-30 14:04:31 +0100

    Add files via upload

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import {cart, removeFromCart} from '../data/cart.js';
+import {cart, removeFromCart, updateDeliveryOption} from '../data/cart.js';
 import {products} from '../data/products.js';
 import {formatCurrency} from './utils/money.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
@@ -23,12 +23,29 @@ cart.forEach( (cartItem) => {
     }
   });
 
-  console.log(matchingProduct);
+  const deliveryOptionId = cartItem.deliveryOptionId;
+
+  let deliveryOption;
+
+  deliveryOptions.forEach( (option) => {
+    if (option.id === deliveryOptionId) {
+      deliveryOption = option
+    }
+  });
+
+  const today = dayjs();
+  const deliveryDate = today.add(
+    deliveryOption.deliveryDays, 'days'
+  );
+  const dateString = deliveryDate.format(
+    'dddd, MMMM D'
+  );
+
   cartSummaryHTML +=`
     <div class="cart-item-container 
       js-cart-item-container-${matchingProduct.id}">
       <div class="delivery-date">
-        Delivery date: Tuesday, June 21
+        Delivery date: ${dateString}
       </div>
 
       <div class="cart-item-details-grid">
@@ -130,6 +147,15 @@ document.querySelectorAll('.js-delete-link')
       container.remove();
     });
   });
+
+document.querySelectorAll('.js-delivery-option')
+  .forEach( (element) => {
+    element.addEventListener('click', () =>{
+      const {productId, deliveryOptionId} = element.dataset;
+      updateDeliveryOption(productId, deliveryOptionId);
+    });
+  });
+  
 
 
 
