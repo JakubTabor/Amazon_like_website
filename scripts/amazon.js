2271fdc7383b539e325c096c99cb47023b673a86
Jakub <124591894+JakubTabor@users.noreply.github.com>
2024-05-09 14:20:26 +0100

    Add files via upload

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,6 +1,6 @@
 import {cart, addToCart} from '../data/cart.js';
 import {products} from '../data/products.js';
-import {formatCurrency} from './utils/money.js'
+import {formatCurrency} from './utils/money.js';
 
 let productsHTML = '';
 
@@ -26,7 +26,7 @@ products.forEach( (product) => {
       </div>
 
       <div class="product-price">
-        ${(product.priceCents / 100).toFixed(2)} 
+        ${formatCurrency(product.priceCents)} 
       </div>
 
       <div class="product-quantity-container">
@@ -65,20 +65,20 @@ document.querySelector('.js-products-grid')
 
 function updateCartQuantity() {
   let cartQuantity = 0;
-      
-      cart.forEach( (cartItem) => {
-        cartQuantity += cartItem.quantity;
-      });
 
-      document.querySelector('.js-cart-quantity')
-        .innerHTML = cartQuantity; 
+  cart.forEach((cartItem) => {
+    cartQuantity += cartItem.quantity;
+  });
+
+  document.querySelector('.js-cart-quantity')
+    .innerHTML = cartQuantity;
 }
 
 document.querySelectorAll('.js-add-to-cart')
-  .forEach( (button) => {
+  .forEach((button) => {
     button.addEventListener('click', () => {
       const productId = button.dataset.productId;
       addToCart(productId);
       updateCartQuantity();
+    });
   });
-});
