f55f06b2b7c65f57a0ce22fab5633c17d231fa77
Jakub <124591894+JakubTabor@users.noreply.github.com>
2024-06-11 16:37:46 +0100

    Add files via upload

--- data/products.js
+++ data/products.js
@@ -34,6 +34,10 @@ class Product {
   getPrice() {
     return `$${formatCurrency(this.priceCents)}`;
   }
+
+  extraInfoHTML() {
+    return '';
+  }
 }
 
 class Clothing extends Product {
@@ -42,6 +46,13 @@ class Clothing extends Product {
   constructor(productDetails) {
     super(productDetails);
     this.sizeChartLink = productDetails.sizeChartLink
+  }
+
+  extraInfoHTML() {
+    return `
+      <a href="${this.sizeChartLink}" target="_blank">
+      Size chart</a>
+    `;
   }
 }
 const tshirt = new Clothing({
@@ -739,5 +750,8 @@ export const products = [
     ]
   }
 ].map((productDetails) =>{
+  if (productDetails.type === 'clothing') {
+    return new Clothing(productDetails);
+  }
   return new Product(productDetails);
 });
