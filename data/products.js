12b84091f5df46d59541d12357abd026ff257c38
Jakub <124591894+JakubTabor@users.noreply.github.com>
2024-06-12 12:00:05 +0100

    Add files via upload

--- data/products.js
+++ data/products.js
@@ -55,40 +55,20 @@ class Clothing extends Product {
     `;
   }
 }
-const tshirt = new Clothing({
-  id: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
-  image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-  name: "Adults Plain Cotton T-Shirt - 2 Pack",
-  rating: {
-    stars: 4.5,
-    count: 56
-  },
-  priceCents: 799,
-  keywords: [
-    "tshirts",
-    "apparel",
-    "mens"
-  ],
-  type: "clothing",
-  sizeChartLink: "images/clothing-size-chart.png"
-},
-{
-  id: "54e0eccd-8f36-462b-b68a-8182611d9add",
-  image: "images/products/black-2-slot-toaster.jpg",
-  name: "2 Slot Toaster - Black",
-  rating: {
-    stars: 5,
-    count: 2197
-  },
-  priceCents: 1899,
-  keywords: [
-    "toaster",
-    "kitchen",
-    "appliances"
-  ]
-});
-console.log(tshirt);
-console.log(tshirt.getPrice());
+
+function logThis() {
+  console.log(this);
+}
+logThis();
+logThis.call('hello');
+
+this
+const object3 = {
+  method: () => {
+    console.log(this);
+  } 
+};
+object3.method();
 
 export const products = [
   {
