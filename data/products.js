8dfe50501158348c54622075d1a65e233e0aa802
Jakub <124591894+JakubTabor@users.noreply.github.com>
2024-06-15 12:05:04 +0100

    Add files via upload

--- data/products.js
+++ data/products.js
@@ -56,20 +56,29 @@ class Clothing extends Product {
   }
 }
 
-function logThis() {
-  console.log(this);
+export let products = [];
+
+export function loadProducts(fun) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener('load', () => {
+    products = JSON.parse(xhr.response).map((productDetails) => {
+      if (productDetails.type === 'clothing') {
+        return new Clothing(productDetails);
+      }
+      return new Product(productDetails);
+    });
+
+    console.log('load products');
+
+    fun();
+  });
+
+  xhr.open('GET', 'https://supersimplebackend.dev/products');
+  xhr.send();
 }
-logThis();
-logThis.call('hello');
 
-this
-const object3 = {
-  method: () => {
-    console.log(this);
-  } 
-};
-object3.method();
-
+/*
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -735,3 +744,4 @@ export const products = [
   }
   return new Product(productDetails);
 });
+*/
