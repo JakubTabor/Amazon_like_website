7231cb45903e3833ab816500d8134aa05a4d6eb7
Jakub <124591894+JakubTabor@users.noreply.github.com>
2024-06-16 20:32:59 +0100

    Add files via upload

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,9 @@
-export let cart = JSON.parse(localStorage.getItem('cart'));
+export let cart;
+
+loadFromStorage();
+
+export function loadFromStorage() {
+  cart = JSON.parse(localStorage.getItem('cart'));
 
 if (!cart) {
   cart = [{ productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
@@ -9,6 +14,7 @@ if (!cart) {
    quantity: 1,
    deliveryOptionId: '2'
   }]; 
+}
 }
 
 
@@ -65,4 +71,17 @@ export function updateDeliveryOption(productId, deliveryOptionId) {
   matchingItem.deliveryOptionId = deliveryOptionId;
 
   saveToStorage();
+}
+
+export function loadCart(fun) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener('load', () => {
+    console.log(xhr.response);
+
+    fun();
+  });
+
+  xhr.open('GET', 'https://supersimplebackend.dev/cart');
+  xhr.send();
 }
