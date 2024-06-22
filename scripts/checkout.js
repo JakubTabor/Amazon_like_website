ad9f19f1a4fef8b29063bbffa621e6452ddb56bf
Jakub <124591894+JakubTabor@users.noreply.github.com>
2024-06-22 12:46:41 +0100

    Add files via upload

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,18 +1,67 @@
 import {renderOrderSummary} from './checkout/orderSummary.js';
 import {renderPaymentSummary} from './checkout/paymentSummary.js';
 import {loadProducts, loadProductsFetch} from '../data/products.js';
-import {loadCart} from '../data/cart.js';
+import {loadCart, loadCartFetch} from '../data/cart.js';
 
 async function loadPage() {
-  await loadProductsFetch();
+  try {
 
-  await new Promise((resolve) => {
-    loadCart(() => {
-      resolve();
-    });
-  });
+    await Promise.all([
+      loadProductsFetch(),
+      loadCartFetch()
+    ]);
+
+  } catch (error) {
+    console.log('Unexpected error. Please try again later.');
+  }
 
   renderOrderSummary();
   renderPaymentSummary();
 }
 loadPage()
+
+/*
+Promise.all([
+  loadProductsFetch(),
+  new Promise( (resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  })
+
+]).then((values) => {
+  console.log(values);
+  renderOrderSummary();
+  renderPaymentSummary();
+});
+*/
+
+/*
+new Promise((resolve) => {
+  loadProducts( () => {
+    resolve('value1');
+  });
+
+}).then((value) => {
+  console.log(value);
+
+  return new Promise( (resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  });
+
+}).then(() => {
+  renderOrderSummary();
+  renderPaymentSummary();
+});
+*/
+
+/*
+loadProducts(() => {
+  loadCart( () => {
+    renderOrderSummary();
+    renderPaymentSummary();
+  });
+});
+*/
