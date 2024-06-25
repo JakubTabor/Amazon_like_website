6087d0894ed66a45caba8172441edb14d260b5cf
Jakub <124591894+JakubTabor@users.noreply.github.com>
2024-06-25 21:54:28 +0100

    Add files via upload

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -7,20 +7,28 @@ loadProducts(renderProductsGrid);
 function renderProductsGrid() {
 
   let productsHTML = '';
+  
+  const url = new URL(window.location.href);
+  const search = url.searchParams.get('search');
 
+  let filteredProducts = products;
 
-  products.forEach( (product) => {
+  if (search) {
+    filteredProducts = products.filter((product) => {
+      return product.name.includes(search);
+    });
+  }
+
+    filteredProducts.forEach((product) => {
     productsHTML += `
       <div class="product-container">
         <div class="product-image-container">
           <img class="product-image"
             src="${product.image}">
         </div>
-
         <div class="product-name limit-text-to-2-lines">
           ${product.name}
         </div>
-
         <div class="product-rating-container">
           <img class="product-rating-stars"
             src="${product.getStarsUrl()}">
@@ -28,11 +36,9 @@ function renderProductsGrid() {
             ${product.rating.count}
           </div>
         </div>
-
         <div class="product-price">
           ${product.getPrice()}
         </div>
-
         <div class="product-quantity-container">
           <select>
             <option selected value="1">1</option>
@@ -47,35 +53,29 @@ function renderProductsGrid() {
             <option value="10">10</option>
           </select>
         </div>
-
         ${product.extraInfoHTML()}
-
         <div class="product-spacer"></div>
-
         <div class="added-to-cart">
           <img src="images/icons/checkmark.png">
           Added
         </div>
-
-        <button class="add-to-cart-button 
-        button-primary js-add-to-cart"
+        <button class="add-to-cart-button button-primary js-add-to-cart"
         data-product-id="${product.id}">
           Add to Cart
         </button>
       </div>
     `;
-  }); 
+  });
+
   document.querySelector('.js-products-grid')
   .innerHTML = productsHTML;
 
 
   function updateCartQuantity() {
     let cartQuantity = 0;
-
     cart.forEach((cartItem) => {
       cartQuantity += cartItem.quantity;
     });
-
     document.querySelector('.js-cart-quantity')
       .innerHTML = cartQuantity;
   }
@@ -87,5 +87,11 @@ function renderProductsGrid() {
         addToCart(productId);
         updateCartQuantity();
       });
+    });
+
+  document.querySelector('.js-search-button')
+    .addEventListener('click', () => {
+      const search = document.querySelector('.js-search-bar').value;
+      window.location.href = `index.html?search=${search}`;
     });
 }
