96be10b5e7b3938aa05d0beaf6299dda477a18f8
Jakub <124591894+JakubTabor@users.noreply.github.com>
2024-06-25 14:11:23 +0100

    Add files via upload

--- scripts/tracking.js
+++ scripts/tracking.js
@@ -19,6 +19,11 @@ async function loadPage() {
     }
   });
 
+  const today = dayjs();
+  const orderTime = dayjs(order.orderTime);
+  const deliveryTime = dayjs(productDetails.estimatedDeliveryTime);
+  const percentProgress = ((today - orderTime) / (deliveryTime - orderTime)) * 100;
+  
   const trackingHTML = `
     <a class="back-to-orders-link link-primary" href="orders.html">
       View all orders
@@ -41,19 +46,26 @@ async function loadPage() {
     <img class="product-image" src="${product.image}">
 
     <div class="progress-labels-container">
-      <div class="progress-label">
-        Preparing
+      <div class="progress-label ${
+        percentProgress < 50 ? 'current-status' : ''
+      }">
+        
       </div>
-      <div class="progress-label current-status">
-        Shipped
+      <div class="progress-label ${
+        (percentProgress >= 50 && percentProgress < 100 ? 'current-status' : '')
+      }">
+        
       </div>
-      <div class="progress-label">
-        Delivered
+      <div class="progress-label ${
+        percentProgress >= 100 ? "current-status" : ''
+      }">
+        
       </div>
     </div>
 
     <div class="progress-bar-container">
-      <div class="progress-bar"></div>
+      <div class="progress-bar" style="width: ${percentProgress}%;">
+      </div>
     </div>
   `;
 
@@ -61,3 +73,4 @@ async function loadPage() {
 }
 
 loadPage();
+
