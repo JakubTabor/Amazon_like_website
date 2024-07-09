ab967fb50075c6c2414036c877471c89afa009a4
Jakub <124591894+JakubTabor@users.noreply.github.com>
2024-07-09 17:41:15 +0100

    Update orders.js

--- scripts/orders.js
+++ scripts/orders.js
@@ -6,50 +6,41 @@ import {addToCart} from '../data/cart.js';
 
 async function loadPage() {
   await loadProductsFetch();
-
   let ordersHTML = '';
-
   orders.forEach((order) => {
-    const orderTimeString  = dayjs(order.orderTime).format('MMMM D');
-
+    const orderTimeString = dayjs(order.orderTime).format('MMMM D');
     ordersHTML += `
-    <div class="order-container">      
-      <div class="order-header">
-        <div class="order-header-left-section">
-          <div class="order-date">
-            <div class="order-header-label">Order Placed:</div>
-            <div>${orderTimeString}</div>
+      <div class="order-container">
+        <div class="order-header">
+          <div class="order-header-left-section">
+            <div class="order-date">
+              <div class="order-header-label">Order Placed:</div>
+              <div>${orderTimeString}</div>
+            </div>
+            <div class="order-total">
+              <div class="order-header-label">Total:</div>
+              <div>$${formatCurrency(order.totalCostCents)}</div>
+            </div>
           </div>
-          <div class="order-total">
-            <div class="order-header-label">Total:</div>
-            <div>$${formatCurrency(order.totalCostCents)}</div>
+          <div class="order-header-right-section">
+            <div class="order-header-label">Order ID:</div>
+            <div>${order.id}</div>
           </div>
         </div>
-
-        <div class="order-header-right-section">
-          <div class="order-header-label">Order ID:</div>
-          <div>${order.id}</div>
+        <div class="order-details-grid">
+          ${productsListHTML(order)}
         </div>
       </div>
-
-      <div class="order-details-grid">
-      ${productsListHTML(order)}
-      </div>
-    </div>
     `;
-  }); 
-
+  });
   function productsListHTML(order) {
     let productsListHTML = '';
-
     order.products.forEach((productDetails) => {
       const product = getProduct(productDetails.productId);
-
       productsListHTML += `
-      <div class="product-image-container">
+        <div class="product-image-container">
           <img src="${product.image}">
         </div>
-
         <div class="product-details">
           <div class="product-name">
             ${product.name}
@@ -68,7 +59,6 @@ async function loadPage() {
             <span class="buy-again-message">Buy it again</span>
           </button>
         </div>
-
         <div class="product-actions">
           <a href="tracking.html?orderId=${order.id}&productId=${product.id}">
             <button class="track-package-button button-secondary">
@@ -78,16 +68,17 @@ async function loadPage() {
         </div>
       `;
     });
-
     return productsListHTML;
   }
 
   document.querySelector('.js-orders-grid').innerHTML = ordersHTML;
 
-  document.querySelectorAll('.js-buy-again').forEach((button) =>{
+  document.querySelectorAll('.js-buy-again').forEach((button) => {
     button.addEventListener('click', () => {
       addToCart(button.dataset.productId);
 
+      // (Optional) display a message that the product was added,
+      // then change it back after a second.
       button.innerHTML = 'Added';
       setTimeout(() => {
         button.innerHTML = `
