ef0c0e55bdc0daaa3f122edd74fe5243113e05a9
Jakub <124591894+JakubTabor@users.noreply.github.com>
2024-07-09 17:10:00 +0100

    Update amazon.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,34 +1,27 @@
 import {cart, addToCart} from '../data/cart.js';
 import {products, loadProducts} from '../data/products.js';
 import {formatCurrency} from './utils/money.js';
-
 loadProducts(renderProductsGrid);
-
 function renderProductsGrid() {
-
   let productsHTML = '';
-  
   const url = new URL(window.location.href);
   const search = url.searchParams.get('search');
-
   let filteredProducts = products;
-
+  // If a search exists in the URL parameters,
+  // filter the products that match the search.
   if (search) {
     filteredProducts = products.filter((product) => {
       let matchingKeyword = false;
-
       product.keywords.forEach((keyword) => {
         if (keyword.toLowerCase().includes(search.toLowerCase())) {
           matchingKeyword = true;
         }
       });
-
       return matchingKeyword ||
-       product.name.toLowerCase().includes(search.toLowerCase());
+        product.name.toLowerCase().includes(search.toLowerCase());
     });
   }
-
-    filteredProducts.forEach((product) => {
+  filteredProducts.forEach((product) => {
     productsHTML += `
       <div class="product-container">
         <div class="product-image-container">
@@ -75,11 +68,7 @@ function renderProductsGrid() {
       </div>
     `;
   });
-
-  document.querySelector('.js-products-grid')
-  .innerHTML = productsHTML;
-
-
+  document.querySelector('.js-products-grid').innerHTML = productsHTML;
   function updateCartQuantity() {
     let cartQuantity = 0;
     cart.forEach((cartItem) => {
@@ -88,7 +77,6 @@ function renderProductsGrid() {
     document.querySelector('.js-cart-quantity')
       .innerHTML = cartQuantity;
   }
-
   document.querySelectorAll('.js-add-to-cart')
     .forEach((button) => {
       button.addEventListener('click', () => {
@@ -97,18 +85,18 @@ function renderProductsGrid() {
         updateCartQuantity();
       });
     });
-
   document.querySelector('.js-search-button')
     .addEventListener('click', () => {
       const search = document.querySelector('.js-search-bar').value;
-      window.location.href = `index.html?search=${search}`;
+      window.location.href = `amazon.html?search=${search}`;
     });
 
+  // Extra feature: searching by pressing "Enter" on the keyboard.
   document.querySelector('.js-search-bar')
     .addEventListener('keydown', (event) => {
       if (event.key === 'Enter') {
         const searchTerm = document.querySelector('.js-search-bar').value;
-        window.location.href = `index.html?search=${searchTerm}`;
+        window.location.href = `amazon.html?search=${searchTerm}`;
       }
     });
 }
